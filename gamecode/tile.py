ac7fb9fb7b196c0caf3821175a01a77194d3bde7
Alex Schmidt <alex.schmidt767@gmail.com>
2022-09-19 19:15:44 -0500

    map now shows on screen

--- gamecode/tile.py
+++ gamecode/tile.py
@@ -5,5 +5,5 @@ class Tile(pygame.sprite.Sprite):
 
     def __init__(self, pos, groups) -> None:
         super().__init__(groups)
-        self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
+        self.image = pygame.image.load('./graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
